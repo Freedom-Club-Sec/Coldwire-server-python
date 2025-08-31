c8a3afb511eacea99e528520b0a4ce963f386040
chadsec1 <freakah1@gmail.com>
2025-08-31 05:49:24 +0300

    feat: protect SMP question

--- app/logic/smp.py
+++ app/logic/smp.py
@@ -13,7 +13,7 @@ redis_client = get_redis()
 # and to actually have code with no surpises that is easily to audit - even if its a bit repetitive,
 # as the SMP implementation is a very important part of Coldwire's security.
 
-def initiate_new_smp(user_id: str, recipient_id: str, question: str, nonce: str, public_key: str) -> None:
+def initiate_new_smp(user_id: str, recipient_id: str, nonce: str, signing_public_key: str) -> None:
     if not check_user_exists(recipient_id):
         raise ValueError("Recipient_id does not exist")
   
@@ -23,14 +23,13 @@ def initiate_new_smp(user_id: str, recipient_id: str, question: str, nonce: str,
     redis_client.rpush(recipient_id, json.dumps({
         "sender": user_id,
         "step": 1,
-        "question": question,
         "nonce": nonce,
-        "public_key": public_key,
+        "signing_public_key": signing_public_key,
         "data_type": "smp"
     }))
 
 
-def smp_step_2_processor(user_id: str, recipient_id: str, proof: str, nonce: str, public_key: str) -> None:
+def smp_step_2_processor(user_id: str, recipient_id: str, nonce: str, signing_public_key: str, question_public_key: str) -> None:
     if not check_user_exists(recipient_id):
         raise ValueError("Recipient_id does not exist")
   
@@ -40,14 +39,47 @@ def smp_step_2_processor(user_id: str, recipient_id: str, proof: str, nonce: str
     redis_client.rpush(recipient_id, json.dumps({
         "sender": user_id,
         "step": 2,
-        "proof": proof,
         "nonce": nonce,
-        "public_key": public_key,
+        "signing_public_key": signing_public_key,
+        "question_public_key": question_public_key,
         "data_type": "smp"
     }))
 
 
-def smp_step_3_processor(user_id: str, recipient_id: str, proof: str) -> None:
+def smp_step_3_processor(user_id: str, recipient_id: str, question_pads_ciphertext: str, question_ciphertext: str) -> None:
+    if not check_user_exists(recipient_id):
+        raise ValueError("Recipient_id does not exist")
+  
+    # Delete any old SMP messages sent to the recipient by the user
+    delete_old_data(recipient_id, "smp", user_id)
+
+    redis_client.rpush(recipient_id, json.dumps({
+        "sender": user_id,
+        "step": 3,
+        "question_pads_ciphertext": question_pads_ciphertext,
+        "question_ciphertext": question_ciphertext,
+        "data_type": "smp"
+    }))
+
+
+
+def smp_step_4_processor(user_id: str, recipient_id: str, proof: str) -> None:
+    if not check_user_exists(recipient_id):
+        raise ValueError("Recipient_id does not exist")
+  
+    # Delete any old SMP messages sent to the recipient by the user
+    delete_old_data(recipient_id, "smp", user_id)
+
+    redis_client.rpush(recipient_id, json.dumps({
+        "sender": user_id,
+        "step": 4,
+        "proof": proof,
+        "data_type": "smp"
+    }))
+
+
+
+def smp_step_5_processor(user_id: str, recipient_id: str, proof: str) -> None:
     if not check_user_exists(recipient_id):
         raise ValueError("Recipient_id does not exist")
   
@@ -57,7 +89,23 @@ def smp_step_3_processor(user_id: str, recipient_id: str, proof: str) -> None:
 
     redis_client.rpush(recipient_id, json.dumps({
         "sender": user_id,
-        "step": 3,
+        "step": 5,
+        "proof": proof,
+        "data_type": "smp"
+    }))
+
+
+def smp_step_6_processor(user_id: str, recipient_id: str, proof: str) -> None:
+    if not check_user_exists(recipient_id):
+        raise ValueError("Recipient_id does not exist")
+  
+    # Delete any old SMP messages sent to the recipient by the user
+    delete_old_data(recipient_id, "smp", user_id)
+
+
+    redis_client.rpush(recipient_id, json.dumps({
+        "sender": user_id,
+        "step": 6,
         "proof": proof,
         "data_type": "smp"
     }))
--- app/routes/smp.py
+++ app/routes/smp.py
@@ -3,34 +3,48 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel, validator
 from base64 import b64encode, b64decode
 from app.core.crypto import verify_signature
-from app.logic.smp import initiate_new_smp, smp_step_2_processor, smp_step_3_processor, smp_failure_processor
+from app.logic.smp import (
+        initiate_new_smp, 
+        smp_step_2_processor, 
+        smp_step_3_processor, 
+        smp_step_4_processor, 
+        smp_step_5_processor, 
+        smp_step_6_processor, 
+        smp_failure_processor)
 from app.utils.helper_utils import valid_b64, valid_hex
 from app.utils.jwt import verify_jwt_token
 from app.core.constants import (
     SMP_NONCE_LENGTH,
     SMP_PROOF_LENGTH,
     SMP_QUESTION_MAX_LEN,
-    ML_DSA_87_PK_LEN
+    ML_DSA_87_PK_LEN,
+    ML_KEM_1024_PK_LEN
 )
 import asyncio
 
 router = APIRouter()
 
 class InitiateSMP(BaseModel):
-    question: str
     nonce: str
-    public_key: str
+    signing_public_key: str
     recipient: str
 
 
 class SMP_2(BaseModel):
-    proof: str
     nonce: str
-    public_key: str
+    signing_public_key: str
+    question_public_key: str
     recipient: str
 
 
 class SMP_3(BaseModel):
+    question_ciphertext: str
+    question_pads_ciphertext: str
+    recipient: str
+
+
+
+class SMP_Proof(BaseModel):
     proof: str
     recipient: str
 
@@ -41,9 +55,8 @@ class SMP_Failure(BaseModel):
 
 @router.post("/smp/initiate")
 async def smp_initiate(payload: InitiateSMP, response: Response, user=Depends(verify_jwt_token)):
-    question   = payload.question
     nonce      = payload.nonce
-    public_key = payload.public_key
+    signing_public_key = payload.signing_public_key
     recipient  = payload.recipient
 
     user_id = user["id"]
@@ -54,12 +67,12 @@ async def smp_initiate(payload: InitiateSMP, response: Response, user=Depends(ve
     if (not recipient.isdigit()) or len(recipient) != 16:
         raise HTTPException(status_code=400, detail="Invalid recipient")
 
-    if (not valid_b64(public_key)) or len(b64decode(public_key)) != ML_DSA_87_PK_LEN:
+    if (not valid_b64(signing_public_key)) or len(b64decode(signing_public_key)) != ML_DSA_87_PK_LEN:
         raise HTTPException(status_code=400, detail="Malformed public_key")
 
 
     try:
-        await asyncio.to_thread(initiate_new_smp, user_id, recipient, question, nonce, public_key)
+        await asyncio.to_thread(initiate_new_smp, user_id, recipient, nonce, signing_public_key)
     except ValueError as e:
         return {"status": "failure", "error": e}
 
@@ -69,9 +82,9 @@ async def smp_initiate(payload: InitiateSMP, response: Response, user=Depends(ve
 
 @router.post("/smp/step_2")
 async def smp_step_2(payload: SMP_2, response: Response, user=Depends(verify_jwt_token)):
-    proof      = payload.proof
     nonce      = payload.nonce
-    public_key = payload.public_key
+    signing_public_key = payload.signing_public_key
+    question_public_key = payload.question_public_key
     recipient  = payload.recipient
 
     user_id = user["id"]
@@ -80,20 +93,47 @@ async def smp_step_2(payload: SMP_2, response: Response, user=Depends(verify_jwt
         raise HTTPException(status_code=400, detail="Malformed nonce")
   
 
-    # HMAC SHA512 is fixed-size to 64 bytes
-    if (not valid_hex(proof)) or len(bytes.fromhex(proof)) != SMP_PROOF_LENGTH:
-        raise HTTPException(status_code=400, detail="Malformed proof")
-  
     if (not recipient.isdigit()) or len(recipient) != 16:
         raise HTTPException(status_code=400, detail="Invalid recipient")
 
 
-    if (not valid_b64(public_key)) or len(b64decode(public_key)) != ML_DSA_87_PK_LEN:
-        raise HTTPException(status_code=400, detail="Malformed public_key")
+    if (not valid_b64(signing_public_key)) or len(b64decode(signing_public_key)) != ML_DSA_87_PK_LEN:
+        raise HTTPException(status_code=400, detail="Malformed signing_public_key")
+
+    if (not valid_b64(question_public_key)) or len(b64decode(question_public_key)) != ML_KEM_1024_PK_LEN:
+        raise HTTPException(status_code=400, detail="Malformed question_public_key")
 
 
     try:
-        await asyncio.to_thread(smp_step_2_processor, user_id, recipient, proof, nonce, public_key)
+        await asyncio.to_thread(smp_step_2_processor, user_id, recipient, nonce, signing_public_key, question_public_key)
+    except ValueError as e:
+        return {"status": "failure", "error": e}
+
+    return {"status": "success"}
+
+
+@router.post("/smp/step_3")
+async def smp_step_3(payload: SMP_3, response: Response, user=Depends(verify_jwt_token)):
+    question_pads_ciphertext = payload.question_pads_ciphertext
+    question_ciphertext      = payload.question_ciphertext
+    recipient                = payload.recipient
+
+    user_id = user["id"]
+
+    if (not recipient.isdigit()) or len(recipient) != 16:
+        raise HTTPException(status_code=400, detail="Invalid recipient")
+
+
+    if (not valid_b64(question_pads_ciphertext)): # or len(b64decode(question_pads_ciphertext)) != SMP_QUESTION_MAX_LEN:
+        raise HTTPException(status_code=400, detail="Malformed question_pads_ciphertext")
+
+
+    if (not valid_b64(question_ciphertext)) or len(b64decode(question_ciphertext)) > SMP_QUESTION_MAX_LEN:
+        raise HTTPException(status_code=400, detail="Malformed question_ciphertext")
+
+
+    try:
+        await asyncio.to_thread(smp_step_3_processor, user_id, recipient, question_pads_ciphertext, question_ciphertext)
     except ValueError as e:
         return {"status": "failure", "error": e}
 
@@ -101,8 +141,8 @@ async def smp_step_2(payload: SMP_2, response: Response, user=Depends(verify_jwt
 
 
 
-@router.post("/smp/step_3")
-async def smp_step_3(payload: SMP_3, response: Response, user=Depends(verify_jwt_token)):
+@router.post("/smp/step_4")
+async def smp_step_4(payload: SMP_Proof, response: Response, user=Depends(verify_jwt_token)):
     proof     = payload.proof
     recipient = payload.recipient
 
@@ -115,7 +155,49 @@ async def smp_step_3(payload: SMP_3, response: Response, user=Depends(verify_jwt
         raise HTTPException(status_code=400, detail="Invalid recipient")
 
     try:
-        await asyncio.to_thread(smp_step_3_processor, user_id, recipient, proof)
+        await asyncio.to_thread(smp_step_4_processor, user_id, recipient, proof)
+    except ValueError as e:
+        return {"status": "failure", "error": e}
+
+    return {"status": "success"}
+
+
+@router.post("/smp/step_5")
+async def smp_step_5(payload: SMP_Proof, response: Response, user=Depends(verify_jwt_token)):
+    proof     = payload.proof
+    recipient = payload.recipient
+
+    user_id = user["id"]
+
+    if (not valid_hex(proof)) or len(bytes.fromhex(proof)) != SMP_PROOF_LENGTH:
+        raise HTTPException(status_code=400, detail="Malformed proof")
+  
+    if (not recipient.isdigit()) or len(recipient) != 16:
+        raise HTTPException(status_code=400, detail="Invalid recipient")
+
+    try:
+        await asyncio.to_thread(smp_step_5_processor, user_id, recipient, proof)
+    except ValueError as e:
+        return {"status": "failure", "error": e}
+
+    return {"status": "success"}
+
+
+@router.post("/smp/step_6")
+async def smp_step_5(payload: SMP_Proof, response: Response, user=Depends(verify_jwt_token)):
+    proof     = payload.proof
+    recipient = payload.recipient
+
+    user_id = user["id"]
+
+    if (not valid_hex(proof)) or len(bytes.fromhex(proof)) != SMP_PROOF_LENGTH:
+        raise HTTPException(status_code=400, detail="Malformed proof")
+  
+    if (not recipient.isdigit()) or len(recipient) != 16:
+        raise HTTPException(status_code=400, detail="Invalid recipient")
+
+    try:
+        await asyncio.to_thread(smp_step_6_processor, user_id, recipient, proof)
     except ValueError as e:
         return {"status": "failure", "error": e}
 
@@ -123,7 +205,7 @@ async def smp_step_3(payload: SMP_3, response: Response, user=Depends(verify_jwt
 
 
 @router.post("/smp/failure")
-async def smp_step_3(payload: SMP_Failure, response: Response, user=Depends(verify_jwt_token)):
+async def smp_failure(payload: SMP_Failure, response: Response, user=Depends(verify_jwt_token)):
     recipient = payload.recipient
 
     user_id = user["id"]
